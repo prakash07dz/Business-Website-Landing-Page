3a90e0b4dac355fae8304a1ef455776245549005
Prakash  Parmar <145080515+prakash07dz@users.noreply.github.com>
2024-03-04 09:56:35 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,34 @@
 // navbar icon
 const ham_menu = document.querySelector(".hamburger-menu");
-const nav_header = document.querySelector('.nav-container');
-
-
+const nav_header = document.querySelector(".nav-container");
 
 const toggleNavbar = () => {
-nav_header.classList.toggle("active");
-}
+  nav_header.classList.toggle("active");
+};
 
-ham_menu.addEventListener('click', ()=> toggleNavbar());
+const closeNavbar = () => {
+  nav_header.classList.remove("active");
+};
 
+ham_menu.addEventListener("click", () => toggleNavbar());
 
+// Close the navbar by default
+closeNavbar();
 
 // accordion-list
- 
-  document.addEventListener('DOMContentLoaded', function () {
-    const accordionItems = document.querySelectorAll('.accordion-item');
 
-    accordionItems.forEach(item => {
-      item.addEventListener('click', function () {
-        accordionItems.forEach(innerItem => {
-          if (innerItem !== item) {
-            innerItem.classList.remove('active');
-          }
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  const accordionItems = document.querySelectorAll(".accordion-item");
 
-        item.classList.toggle('active');
+  accordionItems.forEach((item) => {
+    item.addEventListener("click", function () {
+      accordionItems.forEach((innerItem) => {
+        if (innerItem !== item) {
+          innerItem.classList.remove("active");
+        }
       });
+
+      item.classList.toggle("active");
     });
   });
-
+});
